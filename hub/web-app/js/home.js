d8529ecafbbdd9492e0b054f6b9bf7556f5c620d
Rafael Ramos <rrasmo@gmail.com>
2012-09-11 19:50:02 +0200

    Moved javascript of home to home.js

---
+++ hub/web-app/js/home.js
@@ -0,0 +1,105 @@
+
+var map;
+var markersArray = [];
+var infoWindow = new google.maps.InfoWindow();
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(41.6567,-0.8780);
+  var mapOptions = {
+    zoom: 7,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+}
+
+function fetchCompanies(text) {
+  $('#searchText').val(text);
+  //With API HUB JSON get the TIC companies to refresh map
+  $.getJSON('home/queryJSON', {format: "json", text: text}, function(data) { 
+    updateList(data);
+    updateMap(data);
+  });
+}
+
+function updateList(data) {
+  $('#tableBody').html("");
+
+  //make new rows
+  for (i in data) {
+    var company = data[i]
+    $("#tableBody").append("<tr><td><a href='company/show/" + company.id + "'>" + company.name + "</a></td><td>" + company.description + "</td></tr>");
+  }
+}	
+
+function updateMap(data) {
+  //clear markersArray
+  for (i in markersArray) {
+    var item=markersArray[i];
+    if(item.idTimeout){
+      window.clearTimeout(item.idTimeout);
+    }
+    else{
+      item.setMap(null);
+    }
+  }
+  markersArray.length = 0;
+
+  var tooManyMarkers=!(data.length < 20);
+  var latlngbounds = new google.maps.LatLngBounds( );
+
+  //make new markers
+  for (var i = 0; i < data.length; i++) {
+    var company = data[i]
+    if(company.latitude!=null && company.longitude!=null
+    //Temporal: hasta que los datos sean correctos
+    && company.latitude!=1 && company.longitude!=1
+    ){
+      var location = new google.maps.LatLng(company.latitude, company.longitude);
+
+      latlngbounds.extend( location );
+
+      var optionsMarker={position: location, map: map};
+
+      if(!tooManyMarkers){
+        optionsMarker['animation']=google.maps.Animation.DROP;
+        delete optionsMarker['map'];
+      }
+
+      var marker = new google.maps.Marker(optionsMarker);
+      marker.setTitle(company.name);
+      attachClickEvent(marker, company);
+      markersArray.push(marker);
+
+      //Si no hay demasiados pins lo animamos
+      if(!tooManyMarkers){
+        marker.idTimeout = window.setTimeout(function(marker) {
+          return function(){
+            delete marker.idTimeout;
+            marker.setMap(map);
+          }
+        }(marker), i * 200);
+      }
+    }
+  }
+  map.fitBounds( latlngbounds );
+}
+
+function attachClickEvent(marker, company) {
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.content = "<b>" + company.name + "</b> <br>" + company.address
+    infoWindow.open(map, marker);
+  });
+
+}
+
+$(document).ready(function() {
+  $('#searchForm').submit(function() {
+    fetchCompanies($('#searchText').val());
+    return false;
+  });
+
+  initialize();
+  fetchCompanies();
+});
+
